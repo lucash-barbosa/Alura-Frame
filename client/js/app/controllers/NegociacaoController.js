70f76343f87134bf4ebdbab37481e691b4905219
Lucaslkj <lucas565lkj@gmail.com>
2022-06-08 11:55:24 -0300

    Adicionando lista de negociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,20 +6,32 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listanegociacoes = new Listanegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
 
-        let negociacao = new Negociacao(
-        DateHelper.textoParaData(this._inputData.value),
-        this._inputQuantidade.value,
-        this._inputValor.value
-        );
+        this._listanegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+        this._listanegociacoes.negociacoes.length = 0;
 
+        console.log(this._listanegociacoes.negociacoes);
     }
 
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
+    }
+
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = '';
+        this._inputValor.value = '';
+
+        this._inputData.focus();
+    }
 }
---
+++ client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,16 @@
+class Listanegociacoes{
+
+    constructor(){
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao){
+        this._negociacoes.push(negociacao);
+
+    }
+
+    get negociacoes(){
+
+        return [].concat(this._negociacoes);
+    }
+}
