458031950f4e86ab9f45c8d9407ff1c3dbc04ee0
Lucaslkj <lucas565lkj@gmail.com>
2022-06-09 12:55:53 -0300

    Adicionando mensagem de confrmação

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,17 +6,26 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listanegociacoes = new Listanegociacoes();
+        this._listaNegociacoes = new Listanegociacoes();
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new mensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event){
         event.preventDefault();
 
-        this._listanegociacoes.adiciona(this._criaNegociacao());
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = "Negociação adicionada com sucesso";
+        this._mensagemView.update(this._mensagem);
+
         this._limpaFormulario();
-
-
-        console.log(this._listanegociacoes.negociacoes);
     }
 
     _criaNegociacao(){
@@ -28,8 +37,8 @@ class NegociacaoController{
 
     _limpaFormulario(){
         this._inputData.value = '';
-        this._inputQuantidade.value = '';
-        this._inputValor.value = '';
+        this._inputQuantidade.value = '0';
+        this._inputValor.value = '0.0';
 
         this._inputData.focus();
     }
